8843930aef5e4018c1abd23cef1c870d413a0c4e
45deg <45deg@users.noreply.github.com>
2018-02-01 19:17:13 +0900

    UI Enhancement

--- src/main/java/com/github/_45deg/pdfunbinder/MainController.java
+++ src/main/java/com/github/_45deg/pdfunbinder/MainController.java
@@ -4,15 +4,12 @@ import com.github._45deg.pdfunbinder.outline.OutlineBuilder;
 import com.github._45deg.pdfunbinder.outline.OutlineData;
 import com.github._45deg.pdfunbinder.outline.OutlineMapper;
 import com.github._45deg.pdfunbinder.outline.RootOutlineData;
-
 import com.github._45deg.pdfunbinder.pdf.PageExtractor;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfReader;
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.Observable;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.DragEvent;
@@ -20,15 +17,15 @@ import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
 import javafx.stage.DirectoryChooser;
-import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 public class MainController {
+    @FXML
+    public Button submitbutton;
     @FXML
     private Pane root;
     @FXML
@@ -42,36 +39,36 @@ public class MainController {
     private PdfDocument pdfDocument;
 
     @FXML
-    protected void handleSubmitAction(ActionEvent actionEvent) {
+    public void initialize() {
+        openinglabel.setText("Drag & Drop a PDF File.");
+
+        root.setOnDragOver(this::handleDragOver);
+        root.setOnDragDropped(this::handleDragDropped);
+        submitbutton.setOnAction(this::handleSubmitAction);
+    }
+
+    private void handleSubmitAction(ActionEvent actionEvent) {
         if(pdfDocument == null) return;
 
         final File directory = new DirectoryChooser().showDialog(root.getScene().getWindow());
         if(directory != null) {
             String dirPath = directory.getAbsolutePath();
             PageExtractor extractor = new PageExtractor(pdfDocument);
-            for(OutlineData chapter : selectedItems) {
-                String dest = dirPath + File.separator + chapter.getTitle() + ".pdf";
-                try {
+            try {
+                for(OutlineData chapter : selectedItems) {
+                    String dest = dirPath + File.separator + chapter.getTitle() + ".pdf";
                     extractor.writePages(dest, chapter.getStartPage(), chapter.getEndPage(false));
-                } catch (FileNotFoundException e) {
-                    showError(e);
                 }
+            } catch (FileNotFoundException e) {
+                showError(e);
+            } finally {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Process");
+                alert.setHeaderText("Result");
+                alert.setContentText("Exported " + selectedItems.size() + " files.");
+                alert.showAndWait();
             }
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("Process");
-            alert.setContentText("Exported " + selectedItems.size() + " files.");
-            alert.showAndWait();
         }
-    }
-
-    @FXML
-    public void initialize() {
-        openinglabel.setText("Drag & Drop a PDF File.");
-
-        root.setOnDragOver(this::handleDragOver);
-
-        root.setOnDragDropped(this::handleDragDropped);
     }
 
     private void handleDragOver(DragEvent event) {
@@ -97,9 +94,9 @@ public class MainController {
 
                     selectedItems.clear();
                     setEventHandlerToCheckbox((CheckBoxTreeItem<OutlineData>)treeview.getRoot());
-
+                    submitbutton.setDisable(true);
                     openinglabel.setText(in.getName());
-                } catch (IOException e) {
+                } catch (Exception e) {
                     showError(e);
                 }
             });
@@ -107,6 +104,18 @@ public class MainController {
 
         event.setDropCompleted(true);
         event.consume();
+    }
+
+    private void handleItemChecked(CheckBoxTreeItem<OutlineData> item , boolean newValue) {
+        if(newValue) {
+            selectedItems.add(item.getValue());
+        } else {
+            selectedItems.remove(item.getValue());
+        }
+
+        int size = selectedItems.size();
+        selectinglabel.setText(size + " chapter" + (size == 1 ? "" : "s") + " selected.");
+        submitbutton.setDisable(size < 1);
     }
 
     private void showError(Exception e){
@@ -117,19 +126,11 @@ public class MainController {
     }
 
     private void setEventHandlerToCheckbox(CheckBoxTreeItem<OutlineData> item) {
-        item.selectedProperty().addListener((observable, oldValue, newValue) -> {
-            if(newValue) {
-                selectedItems.add(item.getValue());
-            } else {
-                selectedItems.remove(item.getValue());
-            }
-
-            int size = selectedItems.size();
-            selectinglabel.setText(size + " chapter" + (size == 1 ? "" : "s") + " selected.");
-        });
+        item.selectedProperty().addListener((o, oldValue, newValue) -> { handleItemChecked(item, newValue); });
 
         for(TreeItem<OutlineData> child: item.getChildren()) {
             setEventHandlerToCheckbox((CheckBoxTreeItem<OutlineData>) child);
         }
     }
+
 }
